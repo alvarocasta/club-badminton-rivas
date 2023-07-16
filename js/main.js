ed846dffb261ded555ef4617cdebe3eed65d98fd
badmintonrivas <139715781+badmintonrivas@users.noreply.github.com>
2023-07-16 22:23:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         height: '390',
         width: '640',
-        videoId: 'BkbrwJNsxHs',
+        videoId: '4N07wuTMyDw',
         playerVars: {
             controls: 0,
             loop: 1,
